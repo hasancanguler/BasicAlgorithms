d8d6f1365e3453a3040175d806d7d0c033fb64b9
Hasan Can Güler <hasancan.guler@habermetre.com>
2020-10-01 10:39:17 +0300

    firstCommit

---
+++ BasicAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcatEdgeLetters concatEdgeLetters = new ConcatEdgeLetters();
+
+            concatEdgeLetters.Move(new string[] { "I", "have", "a", "nice", "surprise" });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BasicAlgorithms/concatEdgeLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicAlgorithms
+{
+    public class ConcatEdgeLetters
+    {
+        public string[] Move(string[] a)
+        {
+            List<string> result = new List<string>();
+            string word;
+            for (int index = 0; index < a.Length - 1; index++)
+            {
+                word = a[index].Substring(0, 1) + a[index + 1].Substring(a[index + 1].Length - 1, 1);
+                result.Add(word);
+            }
+
+            word = a[a.Length - 1].Substring(0, 1) + a[0].Substring(a[0].Length - 1, 1);
+            result.Add(word);
+            return result.ToArray();
+        }
+    }
+}
